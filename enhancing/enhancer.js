943dc0520a34e71823b1a343be73687bf8fc8154
Amos Rose <amosroseacct@gmail.com>
2020-03-25 16:16:54 -0600

    fail and repair complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,34 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  return {
+    ...item,
+    enhancement:
+      item.enhancement < 20
+        ? item.enhancement + 1
+        : (item.enhancement = 20 ? item.enhancement : item.enhancement)
+  };
 }
 
 function fail(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability:
+      item.enhancement < 15
+        ? item.durability - 5
+        : item.enhancement >= 15
+        ? item.durability - 10
+        : item.durability,
+    enhancement: item.enhancement > 16 ? item.enhancement - 1 : item.enhancement
+  };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,39 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed } = require("./enhancer.js");
 // test away!
+
+it("should run tests", function() {
+  expect(true).toBe(true);
+});
+
+describe("enhancer.js", function() {
+  describe(".repair()", function() {
+    it("should return an item where durability: 100", function() {
+      expect(repair({ name: "name", durability: 10, enhancement: 10 })).toEqual(
+        {
+          name: "name",
+          durability: 100,
+          enhancement: 10
+        }
+      );
+    });
+  });
+});
+
+describe("enhancer.js", function() {
+  describe(".succeed()", function() {
+    it("should return an item where durability: 100", function() {
+      expect(
+        succeed({ name: "name", durability: 10, enhancement: 10 })
+      ).toEqual({
+        name: "name",
+        durability: 10,
+        enhancement: 11
+      });
+    });
+    it("should not increase enhancement more than 20", function() {
+      expect(
+        succeed({ name: "name", durability: 10, enhancement: 20 })
+      ).toEqual({ name: "name", durability: 10, enhancement: 20 });
+    });
+  });
+});
